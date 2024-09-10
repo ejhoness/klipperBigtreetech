918ff5b7a9c7d7eb7f6cb1b396231f3d17faac18
Jessica Hunt <me@jessicahunt.ca>
2024-09-10 11:32:21 -0400

    correct spacing

--- src/rp2040/spi.c
+++ src/rp2040/spi.c
@@ -42,7 +42,8 @@ DECL_ENUMERATION("spi_bus", "spi0c", 2);
 DECL_CONSTANT_STR("BUS_PINS_spi0c", "gpio16,gpio19,gpio18");
 DECL_ENUMERATION("spi_bus", "spi0d", 3);
 DECL_CONSTANT_STR("BUS_PINS_spi0d", "gpio20,gpio23,gpio22");
-
+DECL_ENUMERATION("spi_bus", "spi0e", 4);
+DECL_CONSTANT_STR("BUS_PINS_spi0e", "gpio4,gpio3,gpio2");
 DECL_ENUMERATION("spi_bus", "spi1a", 5);
 DECL_CONSTANT_STR("BUS_PINS_spi1a", "gpio8,gpio11,gpio10");
 DECL_ENUMERATION("spi_bus", "spi1b", 6);
