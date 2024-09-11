3f9a4019a06bf51f25d513ed0c6a38d56daa78d2
ejhoness <72996067+ejhoness@users.noreply.github.com>
2024-09-11 05:54:01 -0300

    Update CatchIP.py

--- klippy/extras/CatchIP.py
+++ klippy/extras/CatchIP.py
@@ -5,15 +5,13 @@ import time
 import traceback
 
 def get_host_ip():
-
     ip='0.0.0.0'
     try:
         s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
         s.connect(('8.8.8.8',80))
         ip=s.getsockname()[0]
-    except:
-        traceback.print_exc()
+        except:
+            traceback.print_exc()
     finally:
-        s.close()
-
-    return ip
+s.close()
+return ip
