286645a09cfd5a6710139ce25cd80ec1e8f1740c
ejhoness <72996067+ejhoness@users.noreply.github.com>
2025-08-26 13:37:42 -0300

    Update ath20.py
    tnks

--- klippy/extras/ath20.py
+++ klippy/extras/ath20.py
@@ -9,9 +9,9 @@ from . import bus
 ######################################################################
 # Compatible Sensors:
 #       AHT10      -    Tested w/ BTT GTR 1.0 MCU on i2c3
-#       AHT20      -  change `INIT`to :[0xBE, 0x08, 0x00], Untested but should work, tnks :D
+#       AHT20      -    Untested but should work, tnks :D
 #       AHT21      -    Tested w/ BTT GTR 1.0 MCU on i2c3
-  #    tnks.   to ath20 'INIT'              :[0xBE, 0x08, 0x00],
+# to ath20 'INIT' :[0xBE, 0x08, 0x00], maybe with this and filename.
 ######################################################################
 
 AHT10_I2C_ADDR= 0x38
