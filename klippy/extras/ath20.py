4fbb87f0e3d3acb6fddc3f41cb8f8aadec55ef7f
ejhoness <72996067+ejhoness@users.noreply.github.com>
2025-09-04 22:53:52 -0300

    Update ath20.py
    change names aht10 AHT10 to *20

--- klippy/extras/ath20.py
+++ klippy/extras/ath20.py
@@ -1,4 +1,4 @@
-# AHT10/AHT20/AHT21 I2c-based humiditure sensor support
+# AHT20/AHT20/AHT21 I2c-based humiditure sensor support
 #
 # Copyright (C) 2023 Scott Mudge <mail@scottmudge.com>
 #
@@ -8,40 +8,40 @@ from . import bus
 
 ######################################################################
 # Compatible Sensors:
-#       AHT10      -    Tested w/ BTT GTR 1.0 MCU on i2c3
+#       AHT20      -    Tested w/ BTT GTR 1.0 MCU on i2c3
 #       AHT20      -    Untested but should work, tnks :D
 #       AHT21      -    Tested w/ BTT GTR 1.0 MCU on i2c3
 # to ath20 'INIT' :[0xBE, 0x08, 0x00], and this and filename.
 ######################################################################
 
-AHT10_I2C_ADDR= 0x38
+AHT20_I2C_ADDR= 0x38
 
-AHT10_COMMANDS = {
+AHT20_COMMANDS = {
     'INIT'              :[0xBE, 0x08, 0x00],
     'MEASURE'           :[0xAC, 0x33, 0x00],
     'RESET'             :[0xBA, 0x08, 0x00]
 }
 
-AHT10_MAX_BUSY_CYCLES= 5
+AHT20_MAX_BUSY_CYCLES= 5
 
-class AHT10:
+class AHT20:
     def __init__(self, config):
         self.printer = config.get_printer()
         self.name = config.get_name().split()[-1]
         self.reactor = self.printer.get_reactor()
         self.i2c = bus.MCU_I2C_from_config(
-            config, default_addr=AHT10_I2C_ADDR, default_speed=100000)
-        self.report_time = config.getint('aht10_report_time',30,minval=5)
+            config, default_addr=AHT20_I2C_ADDR, default_speed=100000)
+        self.report_time = config.getint('aht20_report_time',30,minval=5)
         self.temp = self.min_temp = self.max_temp = self.humidity = 0.
-        self.sample_timer = self.reactor.register_timer(self._sample_aht10)
-        self.printer.add_object("aht10 " + self.name, self)
+        self.sample_timer = self.reactor.register_timer(self._sample_aht20)
+        self.printer.add_object("aht20 " + self.name, self)
         self.printer.register_event_handler("klippy:connect",
                                             self.handle_connect)
         self.is_calibrated  = False
         self.init_sent = False
 
     def handle_connect(self):
-        self._init_aht10()
+        self._init_aht20()
         self.reactor.update_timer(self.sample_timer, self.reactor.NOW)
 
     def setup_minmax(self, min_temp, max_temp):
@@ -67,28 +67,28 @@ class AHT10:
             while is_busy:
                 # Check if we're constantly busy. If so, send soft-reset
                 # and issue warning.
-                if is_busy and cycles > AHT10_MAX_BUSY_CYCLES:
-                    logging.warning("aht10: device reported busy after " +
-                        "%d cycles, resetting device"% AHT10_MAX_BUSY_CYCLES)
+                if is_busy and cycles > AHT20_MAX_BUSY_CYCLES:
+                    logging.warning("aht20: device reported busy after " +
+                        "%d cycles, resetting device"% AHT20_MAX_BUSY_CYCLES)
                     self._reset_device()
                     data = None
                     break
 
                 cycles += 1
                 # Write command for updating temperature+status bit
-                self.i2c.i2c_write(AHT10_COMMANDS['MEASURE'])
+                self.i2c.i2c_write(AHT20_COMMANDS['MEASURE'])
                 # Wait 110ms after first read, 75ms minimum
                 self.reactor.pause(self.reactor.monotonic() + .110)
 
                 # Read data
                 read = self.i2c.i2c_read([], 6)
                 if read is None:
-                    logging.warning("aht10: received data from" +
+                    logging.warning("aht20: received data from" +
                                     " i2c_read is None")
                     continue
                 data = bytearray(read['response'])
                 if len(data) < 6:
-                    logging.warning("aht10: received bytes less than" +
+                    logging.warning("aht20: received bytes less than" +
                                     " expected 6 [%d]"%len(data))
                     continue
 
@@ -98,7 +98,7 @@ class AHT10:
             if is_busy:
                 return False
         except Exception as e:
-            logging.exception("aht10: exception encountered" +
+            logging.exception("aht20: exception encountered" +
                               " reading data: %s"%str(e))
             return False
 
@@ -120,29 +120,29 @@ class AHT10:
             return
 
         # Reset device
-        self.i2c.i2c_write(AHT10_COMMANDS['RESET'])
+        self.i2c.i2c_write(AHT20_COMMANDS['RESET'])
         # Wait 100ms after reset
         self.reactor.pause(self.reactor.monotonic() + .10)
 
-    def _init_aht10(self):
+    def _init_aht20(self):
         # Init device
-        self.i2c.i2c_write(AHT10_COMMANDS['INIT'])
+        self.i2c.i2c_write(AHT20_COMMANDS['INIT'])
         # Wait 100ms after init
         self.reactor.pause(self.reactor.monotonic() + .10)
         self.init_sent = True
 
         if self._make_measurement():
-            logging.info("aht10: successfully initialized, initial temp: " +
+            logging.info("aht20: successfully initialized, initial temp: " +
                          "%.3f, humidity: %.3f"%(self.temp, self.humidity))
 
-    def _sample_aht10(self, eventtime):
+    def _sample_aht20(self, eventtime):
         if not self._make_measurement():
             self.temp = self.humidity = .0
             return self.reactor.NEVER
 
         if self.temp < self.min_temp or self.temp > self.max_temp:
             self.printer.invoke_shutdown(
-                "AHT10 temperature %0.1f outside range of %0.1f:%.01f"
+                "AHT20 temperature %0.1f outside range of %0.1f:%.01f"
                 % (self.temp, self.min_temp, self.max_temp))
 
         measured_time = self.reactor.monotonic()
@@ -160,4 +160,4 @@ class AHT10:
 def load_config(config):
     # Register sensor
     pheater = config.get_printer().lookup_object("heaters")
-    pheater.add_sensor_factory("AHT10", AHT10)
+    pheater.add_sensor_factory("AHT20", AHT20)
